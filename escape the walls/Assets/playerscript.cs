0c58c1ac0f0c59d8f880e40bc2aeb4df8d6b03de
Meleeman01 <meleeman01@live.com>
2015-11-03 03:59:07 -0600

    created a game
    made the gun fire cubes.

---
+++ escape the walls/Assets/playerscript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerscript : MonoBehaviour {
+	public Texture2D winsplash;
+	// Use this for initialization
+	void Start () {
+	GameObject.Find("character");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter()
+	{
+		GUI.DrawTexture (new Rect(0,0, Screen.width, Screen.height),
+		winsplash, ScaleMode.StretchToFill);
+		DestroyImmediate(this);
+	}
+
+
+	
+}
