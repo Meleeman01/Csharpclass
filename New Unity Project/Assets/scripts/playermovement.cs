4546622785f3c8b9da8c4394155d6932f25b3eb1
Meleeman01 <meleeman01@live.com>
2015-11-03 19:11:52 -0600

    started racing game.
    yeah...

---
+++ New Unity Project/Assets/scripts/playermovement.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playermovement : MonoBehaviour {
+
+	public float speed=16.0f, rotationSpeed= 5.5f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(this.transform.rotation.eulerAngles.z>=180||transform.rotation.eulerAngles.z<=-180) 
+		{
+			Debug.Log("flipped over");
+			transform.eulerAngles = new Vector3(0,0,0);
+		}
+
+	
+	}
+
+	void FixedUpdate(){
+		float foreAndAft = Input.GetAxis ("Vertical") * speed;
+		float rotation =
+			Input.GetAxis ("Horizontal") * rotationSpeed;
+		GetComponent<Rigidbody> ().AddRelativeForce (foreAndAft, 0, 0);
+		GetComponent<Rigidbody> ().AddTorque (0, rotation, 0);
+	
+	}
+}
