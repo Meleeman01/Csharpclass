ac90b16f22d1eb883f9864054b404164540a892d
Meleeman01 <meleeman01@live.com>
2015-11-12 22:58:43 -0600

    fixed camera
    used rotate around to get the camera to behave the way I wanted to.

--- New Unity Project/Assets/scripts/CameraScript.cs
+++ New Unity Project/Assets/scripts/CameraScript.cs
@@ -6,25 +6,30 @@ public class CameraScript : MonoBehaviour
 	public float cameraHeight =5.0f;
 	public float cameraDistance=5.0f;
 
+
 	private Transform playerPosition;
-	private float playerRotationY;
-	private float playerRotationX;
+	public float cameraRotationX;
+	public float carRotationX;
+	private float Xlocked=5;
+	private Vector3 carPosition;
 	
 	// Use this for initialization
 	void Start () 
 	{
 		playerPosition=GameObject.Find("Car").transform;
 
-		playerRotationX=GameObject.Find("firstpersoncam").transform.rotation.eulerAngles.x;
-		playerRotationY=GameObject.Find("firstpersoncam").transform.rotation.eulerAngles.y;
 
 
 	}
 	
 	void Update()
 	{
-		playerRotationX=GameObject.Find("firstpersoncam").transform.rotation.eulerAngles.x;
-		playerRotationY=GameObject.Find("firstpersoncam").transform.rotation.eulerAngles.y;
+		
+
+		//find teh car's rotation.
+		carRotationX=GameObject.Find("Car").transform.rotation.eulerAngles.y; //set car's rotation x to a float variable.
+		cameraRotationX=GameObject.Find("firstpersoncam").transform.rotation.eulerAngles.y; 
+		
 
 		
 	}
@@ -32,13 +37,19 @@ public class CameraScript : MonoBehaviour
 	// Update is called once per frame
 	void LateUpdate () 
 	{
+		//find teh car's rotation.
+		carRotationX=GameObject.Find("Car").transform.rotation.eulerAngles.y; //set car's rotation x to a float variable.
+		cameraRotationX=GameObject.Find("firstpersoncam").transform.rotation.eulerAngles.y;
+		cameraRotationX=carRotationX;
 		
-			transform.position=playerPosition.position + new Vector3(-cameraDistance, cameraHeight, cameraDistance);
-			
+		
+
+		
+		transform.position=playerPosition.position +new Vector3(cameraDistance, cameraHeight,cameraDistance);
+		
 		transform.LookAt(playerPosition);
 
+		transform.RotateAround(playerPosition.position,Vector3.up,cameraRotationX-130);
 
-	
-		
 	}
 }
