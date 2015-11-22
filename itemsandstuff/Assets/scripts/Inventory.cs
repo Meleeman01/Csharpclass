0b9cae8243accab91ac3bbe323fbeca8b2694ba7
Meleeman01 <meleeman01@live.com>
2015-11-21 23:52:38 -0600

    came closer to aquiring a item.
    I'm still dealing with a null exception reference error. I can't seem to
    get rid of. still trying to add objects to a list. using the
    gameobject.Find.

--- itemsandstuff/Assets/scripts/Inventory.cs
+++ itemsandstuff/Assets/scripts/Inventory.cs
@@ -1,15 +1,23 @@
 ﻿using UnityEngine;
 using System.Collections;
-
+using System.Collections.Generic;
 public class Inventory : MonoBehaviour {
+
+	public List <GameObject> Items = new List<GameObject>();
+
+
 
 	// Use this for initialization
 	void Start () {
-	
+	Items.Add(GameObject.Find("Pen"));
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+
+	
+
+
 }
---
+++ itemsandstuff/Assets/scripts/objectscript.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class objectscript : MonoBehaviour {
+
+private Transform playerPosition;
+public Inventory listAdd;
+private GameObject object1;
+
+
+	// Use this for initialization
+void Start () {
+		playerPosition=GameObject.Find("Capsule").transform;
+		listAdd=GetComponent<Inventory>();
+		object1=GameObject.Find(this.gameObject.name);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		 
+ 
+   
+ }
+	
+	
+
+	public void OnTriggerEnter()
+	{
+		if(gameObject.tag=="Item")
+		{
+			Debug.Log("Item touched");
+			Debug.Log(object1+"is the object's name.");
+			object1.name.Remove(0,1);
+			Debug.Log(object1);
+			listAdd.Items.Add(GameObject.Find("obbyy"));
+			gameObject.tag="ItemBagged";
+			transform.position=playerPosition.position + new Vector3(5,5,5);
+
+
+		}
+		
+	}
+
+	public void AddToList(){
+
+	}
+}
